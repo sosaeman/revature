fb8c297909f73689fcc74504f4c30b61e7d72b95
sosaeman <35715185+sosaeman@users.noreply.github.com>
2021-09-10 20:54:37 -0400

    added Excercise One

---
+++ ExcerciseOne.java
@@ -0,0 +1,13 @@
+public class ExcerciseOne{
+
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            if(i % 2 == 1){
+            System.out.println(i);
+            }
+            i++;
+        }while(i<=9);
+
+    }
+}
